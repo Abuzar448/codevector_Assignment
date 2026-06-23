5385c3d17a14900806124f6119623a5cbf9dee1e
Abuzar khan <aburaja2654@gmail.com>
2026-06-24 00:19:09 +0530

    Added dynamic porting

--- Backend/index.js
+++ Backend/index.js
@@ -13,7 +13,7 @@ app.use(cors({
   credentials:true,
 }))
 app.use(express.json());
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.use('/api/product',productRoutes);
 
